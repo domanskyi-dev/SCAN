166690313dba58d8e571233fb85a93b0d981ec3d
TamplierX <163639339+TamplierX@users.noreply.github.com>
2025-03-15 01:59:56 +0300

    Update AuthContext.js

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,5 +1,5 @@
 import React, { createContext, useContext, useState, useEffect } from "react";
-import { authService } from "../services/AuthService";
+import { authService } from "../services/authService.js";
 
 
 const AuthContext = createContext();
